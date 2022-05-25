5064b7f840ce24700b88faa931ee8cda44316601
J-IgorSilva <aleatoria8821@gmail.com>
2022-05-25 15:56:52 -0300

    Adicionado Metodos Js

---
+++ Challenge/script.js
@@ -0,0 +1,21 @@
+const inputTexto = document.querySelector(".input-texto");
+
+const mensagem = document.querySelector(".mensagem");
+
+function BotaoEncripitar(){
+    const textoEncripitado = encripitar(inputTexto.value)
+    mensagem.value = textoEncripitado
+    mensagem.style.backgroundImage="none"
+}
+
+function encripitar(stringEncripitada){
+    let matrizCodigo = [["a","ai"],["e","enter"],["i","imes"],["o","ober"],["u","uder"]];
+    stringEncripitada  = stringEncripitada.toLowerCase();
+    
+    for(let i=0;i<matrizCodigo.length;i++){
+        if(stringEncripitada.includes(matrizCodigo[i][0])){
+            stringEncripitada = stringEncripitada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncripitada
+}
